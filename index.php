ac262982db41f4a8901683f139870e2f5ed8d0fb
Lakshman Parameswaran <lparameswaran@g.harvard.edu>
2013-09-15 22:55:07 -0400

    With caption

--- index.php
+++ index.php
@@ -16,25 +16,33 @@
           $('.slideshow').cycle({
               fx:     'shuffle',      // shuffle effect -- like a card deck
               easing: 'easeOutBack',  
-              delay:  -4000           // change image every 4 seconds
-	});
+              delay:  -3000,          // change image every 3 seconds
+              before:  function() {
+                         $('#caption').html(this.alt);
+                      }
+	              });
       });
       </script>
    </head>
    <body>
+      <table style="text-align:center">
+        <tr><td><p style="text-align:left" id="caption"></p></td></tr>
+        <tr><td>
 	<div class="slideshow">
-		<img src="images/self.jpg" />
-		<img src="images/selfAndWife.jpg" />
-		<img src="images/wifePullingEars.jpg" />
-		<img src="images/selfAndKids.jpg" />
-		<img src="images/kids.jpg" />
-		<img src="images/kidsWithADog.jpg" />
-		<img src="images/posing.jpg" />
-		<img src="images/youngerOne.jpg" />
-		<img src="images/familyAtFireplace.jpg" />
-		<img src="images/familyParty.jpg" />
-		<img src="images/familyFormal.jpg" />
+		<img src="images/self.jpg" alt="Calm and Composed" />
+		<img src="images/selfAndWife.jpg" alt="With Wife - Enjoying a meal" />
+		<img src="images/wifePullingEars.jpg" alt="Some happy moments"/>
+		<img src="images/selfAndKids.jpg" alt="With wonderful kids" />
+		<img src="images/kids.jpg" alt="The kids" />
+		<img src="images/kidsWithADog.jpg" alt="They like dogs" />
+		<img src="images/posing.jpg" alt="This one for the camera" />
+		<img src="images/youngerOne.jpg" alt="Future Scientist" />
+		<img src="images/familyAtFireplace.jpg" alt="Enjoying a weekend" />
+		<img src="images/familyParty.jpg" alt="At a party" />
+		<img src="images/familyFormal.jpg" alt="Getting ready for a party" />
 	</div>
+        </td></tr>
+      </table>
    </body>
 </html>
 
